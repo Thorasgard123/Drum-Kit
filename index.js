39249edf402dac00f8f2e7dd28c45319b7b245fa
Thorasgard123 <92989021+Thorasgard123@users.noreply.github.com>
2023-02-15 16:28:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+var noOdBrums=document.querySelectorAll(".drum").length;
+for(let i=0;i<noOdBrums;i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+     var buttonHTML=this.innerHTML;
+     makeSound(buttonHTML);
+     buttonAnimation(buttonHTML);
+     
+  });
+}
+document.addEventListener("keypress",function(event){
+   makeSound(event.key);
+   buttonAnimation(event.key);
+});
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            var audio=new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio=new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio=new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio=new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio=new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio=new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio=new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+            default: console.log(key);
+
+    
+     }
+}
+function buttonAnimation(currentKey){
+  var activeBtn=document.querySelector("."+currentKey);
+  activeBtn.classList.add("pressed");
+  setTimeout(function(){
+    activeBtn.classList.remove("pressed");
+  },100);
+}
